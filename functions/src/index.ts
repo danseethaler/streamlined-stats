43fb4b45853ad0c6cce60af7319dec1c7465cf4a
Dan Seethaler <dan@seethalers.com>
2018-09-03 16:24:24 -0400

    Convert cloud functions to firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,56 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-export const manageUserGroup = functions.firestore
-  .document('/groups/{groupId}/users/{userId}')
-  .onWrite((change, context) => {
-    const {groupId, userId} = context.params;
+admin.initializeApp(functions.config().firebase);
 
-    if (!change.after.exists) {
-      return change.before.ref.parent.parent.parent.parent
-        .collection('users')
-        .doc(userId)
-        .collection('groups')
-        .doc(groupId)
-        .delete()
+export const addNewUserData = functions.auth.user().onCreate(user => {
+  const {uid, email, displayName = ''} = user;
+  console.log('{uid, email, displayName = }', {uid, email, displayName});
+
+  return admin
+    .database()
+    .ref(`/users/${uid}`)
+    .update({
+      uid,
+      email,
+      displayName,
+    });
+});
+
+export const removeDeletedUserData = functions.auth.user().onDelete(({uid}) =>
+  admin
+    .database()
+    .ref(`/users/${uid}`)
+    .remove()
+);
+
+export const addGroupOwner = functions.database
+  .ref(`/groups/{groupId}`)
+  .onCreate(
+    (
+      change: functions.database.DataSnapshot,
+      context: functions.EventContext
+    ) => {
+      const {groupId} = context.params;
+      const {uid} = context.auth;
+      console.log('uid', uid);
+
+      const userGroupData = {
+        status: 'active',
+        role: 'owner',
+      };
+
+      return change
+        .child(`users/${uid}`)
+        .ref.set(userGroupData)
+        .then(() =>
+          admin
+            .database()
+            .ref(`/users/${uid}/groups/${groupId}`)
+            .update(userGroupData)
+        )
         .catch(err => {
           console.log('err', err);
         });
     }
-
-    return change.after.ref.parent.parent.parent.parent
-      .collection('users')
-      .doc(userId)
-      .collection('groups')
-      .doc(groupId)
-      .set(change.after.data())
-      .catch(err => {
-        console.log('err', err);
-      });
-  });
+  );
